be8eabaeb9c36a2d0348f1b1c282c9d0f70b6ce8
David Velasquez <davidvo22@hotmail.com>
2019-12-27 15:00:18 +0100

    Started the youtube tutorial and did the first four parts of the tutorial

--- Source/classes.hpp
+++ Source/classes.hpp
@@ -1,14 +1,127 @@
 #include <iostream>
 
-class point3d{
-    float px;
-    float py;
-    float pz;
+class Vect{
+    double x, y, z;
 
-    point3d();
-    point3d(float x): px(x){}
-    point3d(float x, float y): px(x), py(y){}
-    point3d(float x, float y, float z): px(x), py(y), pz(z){}
-    ~point3d();
+    public:
+    Vect():x(0), y(0), z(0){}
+    Vect(double px): x(px), y(0), z(0){}
+    Vect(double px, double py): x(px), y(py), z(0){}
+    Vect(double px, double py, double pz): x(px), y(py), z(pz){}
 
+    ~Vect();
+
+    //get functions
+    double getVectX() {return x;}
+    double getVectY() {return y;}
+    double getVectZ() {return z;}
+
+    double magnitude (){ return sqrt(x*x + y*y + z*z);}
+
+    Vect normalize () {
+        double magnitude = sqrt(x*x + y*y + z*z);
+        return Vect (x/magnitude,y/magnitude,z/magnitude);
+    }
+
+    Vect negative () { return Vect (-x, -y, -z);}
+
+    double dotProduct(Vect v) {
+        return x*v.getVectX() + y*v.getVectY() + z*v.getVectZ();
+    }
+
+    Vect crossproduct(Vect v){
+        return Vect (y*v.getVectZ()- z*v.getVectY(), - x*v.getVectZ() + z*v.getVectX(), x*v.getVectY() - y*v.getVectX());   }
+
+    Vect vectAdd (Vect v) {
+        return Vect (x + v.getVectX(), y + v.getVectY(), z + v.getVectZ() );
+    }
+
+    Vect VectMul (double scalar) {
+        return Vect(x*scalar, y*scalar, z*scalar);
+    }
+
+};
+
+class Ray{
+    Vect origin, direction;
+
+
+    public:
+    Ray():origin(0,0,0), direction(1,0,0){}
+    Ray(Vect adirection): origin(0,0,0), direction(adirection){}
+    Ray(Vect adirection, Vect anorigin): origin(anorigin), direction(adirection){}
+    ~Ray();
+
+    Vect getRayOrigin() {return origin;}
+    Vect getRayVector() {return direction;}
+
+
+
+
+};
+
+class Camera{
+    Vect campos, camdir, camright, camdown; 
+    public:
+
+    Camera():campos(0,0,0), camdir(0,0,1), camright(0,0,0), camdown(0,0,0){}
+    Camera(Vect acampos):campos(acampos), camdir(1,0,0), camright(0,0,0), camdown(0,0,0){}
+    Camera(Vect acampos, Vect acamdir):campos(acampos), camdir(acamdir), camright(0,0,0), camdown(0,0,0){}
+    Camera(Vect acampos, Vect acamdir, Vect acamright):campos(acampos), camdir(acamdir), camright(acamright), camdown(0,0,0){}
+    Camera(Vect acampos, Vect acamdir, Vect acamright, Vect acamdown):campos(acampos), camdir(acamdir), camright(acamright), camdown(acamdown){}
+    ~Camera();
+
+    Vect getCameraPosition() {return campos;}
+    Vect getCameraDirection() {return camdir;}
+    Vect getcameraRight() {return camright;}
+    Vect getCameraDown() {return camdown;}
+
+};
+
+class color{
+    double red, green, blue, special; //the special value will represent reflectiveness of the surface
+
+    public:
+
+    //constructors
+    color():red(0.5),green(0.5),blue(0.5){}
+    color(double ared, double agreen, double ablue, double aspecial):red(ared), green(agreen), blue(ablue), special(aspecial){}
+
+    //get functions
+    double getColorRed() {return red;}
+    double getColorGreen() {return green;}
+    double getColorBlue() {return blue;}
+    double getColorSpecial() {return special;}
+
+    //set functions
+    double setColorRed(double ared) {red = ared;}
+    double setColorGreen(double agreen) {green = agreen;}
+    double setColorBlue(double ablue) {blue = ablue;}
+    double setColorSpecial(double aspecial) {special = aspecial;}
+
+};
+
+class light{
+        Vect position;
+        color Color;
+
+
+
+    public:
+    light():position(0,0,0), Color(1,1,1,0){}
+    light(Vect aposition): position(aposition), Color(1,1,1,0){}
+    light(Vect aposition, color acolor): position(aposition), Color(acolor){}
+    ~light();
+
+    Vect getLightPosition() {return position;}
+    color getLightColor() {return Color;}
+
+};
+
+struct RGBType{
+    double r;
+    double g;
+    double b;
+
+    
 };
--- Source/main.cpp
+++ Source/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <limits>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#include "classes.hpp"
 
 using namespace std;
 
@@ -40,15 +49,129 @@ void normalize_pixels(float w, float h){
         for (int y=0;y<h;y++){
             X_Camara=(2*((x+0.5)/w)-1)*Image_Ratio*tan(alpha/2);
             Y_Camara=(1-2*((y+0.5)/h))*tan(alpha/2);
-            cout << "( " << x << ", " << y <<" ) -> " << "( " << X_Camara << ", " << Y_Camara<<", -1 )" << endl;
+   //         cout << "( " << x << ", " << y <<" ) -> " << "( " << X_Camara << ", " << Y_Camara<<", -1 )" << endl;
         }
     }
 }
 
 
-int main(){
-    float Width=2, Height=2;
-    normalize_pixels(Width,Height);
+void savebmp (const char *filename, int w, int h, int dpi, RGBType *data ){
+    FILE *f;
+    int k = w*h;
+    int s= 4*k;
+    int filesize = 54 +s;
+
+    double factor = 39.375;
+    int m = static_cast<int>(factor);
+
+    int ppm = dpi*m;
+
+    unsigned char bmpfileheader[14] = {'B','M', 0,0,0,0, 0,0,0,0, 54, 0,0,0};
+    unsigned char bmpinfoheader[40] = {40,0,0,0, 0,0,0,0, 0,0,0,0, 1,0,24,0};
+
+
+    bmpfileheader[ 2] = (unsigned char)(filesize);
+    bmpfileheader[ 3] = (unsigned char)(filesize>>8);
+    bmpfileheader[ 4] = (unsigned char)(filesize>>16);
+    bmpfileheader[ 5] = (unsigned char)(filesize>>24);
+
+    bmpinfoheader[ 4] = (unsigned char)(w);
+    bmpinfoheader[ 5] = (unsigned char)(w>>8);
+    bmpinfoheader[ 6] = (unsigned char)(w>>16);
+    bmpinfoheader[ 7] = (unsigned char)(w>>24);
+
+    bmpinfoheader[ 8] = (unsigned char)(h);
+    bmpinfoheader[ 9] = (unsigned char)(h>>8);
+    bmpinfoheader[ 10] = (unsigned char)(h>>16);
+    bmpinfoheader[ 11] = (unsigned char)(h>>24);
+
+    bmpinfoheader[ 21] = (unsigned char)(s);
+    bmpinfoheader[ 22] = (unsigned char)(s>>8);
+    bmpinfoheader[ 23] = (unsigned char)(s>>16);
+    bmpinfoheader[ 24] = (unsigned char)(s>>24);
+
+    bmpinfoheader[ 25] = (unsigned char)(ppm);
+    bmpinfoheader[ 26] = (unsigned char)(ppm>>8);
+    bmpinfoheader[ 27] = (unsigned char)(ppm>>16);
+    bmpinfoheader[ 28] = (unsigned char)(ppm>>24);
+
+    bmpinfoheader[ 29] = (unsigned char)(ppm);
+    bmpinfoheader[ 30] = (unsigned char)(ppm>>8);
+    bmpinfoheader[ 31] = (unsigned char)(ppm>>16);
+    bmpinfoheader[ 32] = (unsigned char)(ppm>>24);
+
+    f = fopen(filename,"wb");
+
+    fwrite(bmpfileheader,1,14,f);
+    fwrite(bmpinfoheader,1,40,f);
+
+    for (int i = 0; i<k; i++) {
+        RGBType rgb = data[i];
+
+        double red = (data[i].r)*255;
+        double green = (data[i].g)*255;
+        double blue = (data[i].b)*255;
+
+        unsigned char color[3] = {(int)floor(blue),(int)floor(green),(int)floor(red)};
+
+        fwrite(color,1,3,f);
+
+    }
+
+    fclose(f);
+}
+
+int thisone;
+
+int main(int argc, char *argv[]){
+
+    int Width=640, Height=480, dpi = 72;
+    int n = Width*Height;
+    RGBType *pixels = new RGBType[n];
+
+    //coordinate system
+    Vect X (1,0,0);
+    Vect Y (0,1,0);
+    Vect Z (0,0,1);
+
+
+
+    //Camera deffinition
+    Vect campos (3, 1.5, -4);
+    Vect look_at (0,0,0);
+    Vect diff_btw (campos.getVectX() - look_at.getVectX(),campos.getVectY() - look_at.getVectY(),campos.getVectZ()-look_at.getVectZ());
+    Vect camdir = diff_btw.negative().normalize();
+    Vect camright = Y.crossproduct(camdir).normalize();
+    Vect camdown = camright.crossproduct(camdir);
+    Camera scene_cam (campos, camdir, camright, camdown);
+
+    color white_light (1.0,1.0,1.0,0);
+    color pretty_green (0.5,1,0.5,0.3);
+    color gray (0.5, 0.5, 0.5, 0);
+    color black (0,0,0,0);
+
+    Vect light_position (-7,10,-10);
+    light scene_light (light_position, white_light);
+
+
+    // Nested loops to give each pixel a color
+    for (int x = 0; x < Width; x++)     
+    {
+        for (int y = 0; y < Height; y++)
+        {
+            thisone = y*Width + x;      //determine coordenates of an individual pixel
+            if((x > 0 && x < 255) && (y > 0 &&  y < 255)){
+                pixels[thisone].r = x;
+                pixels[thisone].g = y;
+                pixels[thisone].b = 255;
+            }
+
+        }
+        
+    }
+    
+    savebmp("Scene.bmp",Width,Height,dpi,pixels);
+
     return 0;
 }
 
