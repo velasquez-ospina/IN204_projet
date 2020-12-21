0ac358fba45b525aac4f682b3c5d2127350c2f13
David <davidvo22@hotmail.com>
2020-12-21 17:45:13 +0100

    deleted old files

--- ray-tracing-Ajeno/headers/utils/params.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef PARAMS_HPP
-#define PARAMS_HPP
-
-const double EPSILON = 0.0002;
-
-#endif //PARAMS_HPP
--- ray-tracing-Ajeno/headers/utils/parser.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef PARSER_HPP
-#define PARSER_HPP
-
-#include "scene.hpp"
-
-// Json parser that is used to created a scene based on a json
-Scene jsonToScene(std::string path);
-
-#endif // PARSER_HPP
